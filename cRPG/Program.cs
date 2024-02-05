2a696b607aded60fbff86d40e914e6e384fa3616
Fmorejon <morejonff@gmail.com>
2024-02-05 16:32:55 -0600

    addingVars

--- cRPG/Program.cs
+++ cRPG/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿MyClass.TestMethod();
+await Task.Delay(2000);
+MyNamespace.MyClass.MyMethod();
+
+string[] charStatsNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"];
+int[] char1Stats = [0,0,0,0,0,0];
+Random dice = new Random();
+
+
+public class MyClass
+{
+    public static void TestMethod()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public static void MyMethod()
+        {
+            Console.WriteLine("Hello World from MyNamespace.MyClass.MyMethod!");
+        }
+    }
+}
+
+ 
+
+
+
